22a40360f948add31a5d4bff007239620376479e
showerlee <showerlee@vip.qq.com>
2019-02-10 01:11:19 +0800

    Update SetSnapshot

--- script/SetSnaphot.sh
+++ script/SetSnaphot.sh
@@ -7,7 +7,7 @@ VERSION=$3
 PRO_PROPERTIES="promote.properties"
 URL="http://nexus.example.com:8081/repository/Java-war-dev/com/jenkins/demo/Java-war-dev/${VERSION}-SNAPSHOT/maven-metadata.xml"
 
-TIMESTAMP=`curl -s -u $USER:$PASS $URL | grep value | head -n "1" | awk -F'<' '{print $2}'|  awk -F'>' '{print $2}'`
+SNAPSHOT=`curl -s -u $USER:$PASS $URL | grep value | head -n "1" | awk -F'<' '{print $2}'|  awk -F'>' '{print $2}'`
 
 echo "SNAPSHOT=${SNAPSHOT}" >> ${PRO_PROPERTIES}
 
