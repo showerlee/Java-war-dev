7c685ce2b1d87e87661c140d7381b8b60814bf6b
showerlee <showerlee@vip.qq.com>
2019-02-09 22:24:30 +0800

    Update Jenkinsfile

--- script/SetPromoteProperties.sh
+++ script/SetPromoteProperties.sh
@@ -7,5 +7,5 @@ PRO_PROPERTIES="promote.properties"
 VERSION=`cat $POM_FILE |grep "version" | head -n "1" | awk -F'-' '{print \$1}'| sed 's/  <version>//g'`
 APPNAME=`cat pom.xml |grep "artifactId" | head -n "1" | awk -F'>' '{print $2}' | awk -F'<' '{print $1}'`
 
-echo "APPNAME=${APPNAME}" >> ${PRO_PROPERTIES}
-echo "VERSION=${VERSION}" > ${PRO_PROPERTIES}
+echo "APPNAME=${APPNAME}" > ${PRO_PROPERTIES}
+echo "VERSION=${VERSION}" >> ${PRO_PROPERTIES}
