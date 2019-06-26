10e858be03c2d2bff09002c8c1848d6586b0a547
showerlee <showerlee@vip.qq.com>
2019-06-27 01:09:37 +0800

    Update Jenkinsfile

--- script/SetSnapshot.sh
+++ script/SetSnapshot.sh
@@ -7,7 +7,7 @@ VERSION=$3
 PRO_PROPERTIES="promote.properties"
 URL="http://nexus.example.com:8081/repository/Java-war-dev/com/jenkins/demo/Java-war-dev/${VERSION}-SNAPSHOT/maven-metadata.xml"
 
-SNAPSHOT=`curl -s -u $USER:$PASS $URL | grep value | head -n "1" | awk -F'<' '{print $2}'|  awk -F'>' '{print $2}'`
+SNAPSHOT=`curl -s -u $USER:$PASS $URL | xmllint --xpath "//metadata/versioning/snapshotVersions/snapshotVersion[last()]/value/text()" -`
 
 echo "SNAPSHOT=${SNAPSHOT}" >> ${PRO_PROPERTIES}
 
